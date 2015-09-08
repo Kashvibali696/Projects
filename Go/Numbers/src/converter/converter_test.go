3e5d33049dabf689abd8dab16e1a307d54fa4905
mic4ael <crossner90@gmail.com>
2015-09-08 21:14:00 +0200

    Not complete tests for Go programs

--- Go/Numbers/src/converter/converter_test.go
+++ Go/Numbers/src/converter/converter_test.go
@@ -1,1 +1,10 @@
 package converter
+
+import "testing"
+
+func TestGetPiWhenNegativeOrZero(t *testing.T) {
+	value, err := FindPiToNthDigit(-1)
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
--- Go/Numbers/src/pi/pi_to_nth_digit.go
+++ Go/Numbers/src/pi/pi_to_nth_digit.go
@@ -1,5 +1,14 @@
 package pi
 
-func FindPiToNthDigit(digit int) int {
-	return 2
+import "math"
+import "errors"
+
+// Enter a number and have the program generate PI up to that many decimal places.
+// Keep a limit to how far the program will go.
+func FindPiToNthDigit(digit int) (string, error) {
+	if digit <= 0 {
+		return "", errors.New("Wrong argument value!")
+	}
+
+	math.Pi
 }
